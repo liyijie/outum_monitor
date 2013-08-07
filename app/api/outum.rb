0c5cdc432afa23313dba4f376ae5910df91fb6f7
liyijie <liyijie825@gmail.com>
2013-08-07 13:46:39 +0800

    add distance and duration function

--- app/api/outum.rb
+++ app/api/outum.rb
@@ -16,7 +16,6 @@ module Outum
       end
       post ':client_id' do
         time = Time.zone.now
-        date = time.to_date
         # 查找是否有对应client的信息，如果没有则创建一个
         client = Client.find_by_label params[:client_id]
         if client.nil?
@@ -35,24 +34,13 @@ module Outum
         lat = params[:lat]
         lon = params[:lon]
         
-
         # 1. 对于同一天的数据需要更新
         # 2. 更新的时，需要根据之前的结果更新在线时长和里程
         # 3. 如果不存在之前一天的数据，则新建一条数据
-        record = client.records.find_by_date(date)
-        if record.nil?
-          record = client.records.new
-          record.date = date
-          record.distance = 0
-        else
-          # distance = 
-        end
-
+        record = client.records.find_by_date(time.to_date) || client.records.new
         record.ue = ue if ue
         record.imei = imei if imei
-        record.lat = lat.to_f if lat && lon
-        record.lon = lon.to_f if lat && lon
-        record.last_time = time
+        record.update_info lat, lon, time
 
         record.save
       end
--- app/models/record.rb
+++ app/models/record.rb
@@ -3,5 +3,37 @@ class Record < ActiveRecord::Base
 
   belongs_to :client
 
-  
+  def update_info lat, lon, time
+    self.distance ||= 0
+    self.duration ||= 0
+    if lat && lon
+      lng1 = self.lon.to_f
+      lat1 = self.lat.to_f
+      lng2 = lon.to_f
+      lat2 = lat.to_f
+      rad = Math::PI / 180.0
+      earth_radius = 6378.137 * 1000 #地球半径
+      radLat1 = lat1 * rad
+      radLat2 = lat2 * rad
+      a = radLat1 - radLat2
+      b = (lng1 - lng2) * rad
+      s = 2 * Math.asin(Math.sqrt( (Math.sin(a/2)**2) + Math.cos(radLat1) * Math.cos(radLat2)* (Math.sin(b/2)**2) ))
+      s = s * earth_radius
+      s = (s * 10000).round / 10000
+
+      delta_time = self.last_time ? (time - self.last_time) : 0
+      puts "delta_time is:#{delta_time}"
+      puts "s is:#{s}"
+
+      if delta_time > 0 && delta_time < 60 && (s / delta_time < 1000)
+        self.distance += s
+        self.duration += delta_time
+      end
+      self.lat = lat
+      self.lon = lon
+    end
+    self.last_time = time
+    self.date = time.to_date
+  end
+
 end
